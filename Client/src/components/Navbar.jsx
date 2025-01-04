bc85a8e922e291c643613a5b38c547358918fffa
rafael vincent <d749107@gmail.com>
2025-01-04 11:00:00 +0800

    ui changes

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleRefresh = () => {
+    navigate("/")
+  }
 
   return (
     <nav className="fixed top-0 left-0 w-full bg-black/80 backdrop-blur-md text-white px-6 py-4 flex items-center justify-between shadow-md z-50">
       {/* Logo */}
       <div className="text-2xl font-bold text-red-600 tracking-wide cursor-pointer">
-        <Link className="hover:text-red-500 cursor-pointer transition px-6 py-2 md:py-0" to="/">
+        <Link onClick={handleRefresh} className="hover:text-red-500 cursor-pointer transition px-6 py-2 md:py-0" to="/">
           MyFlix
         </Link>
       </div>
@@ -30,7 +34,7 @@ const Navbar = () => {
         } absolute top-full left-0 w-full flex-col bg-black/90 md:bg-transparent md:static md:flex md:flex-row md:space-x-6 text-sm font-medium`}
       >
         <li>
-          <Link className="hover:text-red-500 cursor-pointer transition px-6 py-2 md:py-0" to="/">
+          <Link onClick={handleRefresh} className="hover:text-red-500 cursor-pointer transition px-6 py-2 md:py-0" to="/">
             Home
           </Link>
         </li>
