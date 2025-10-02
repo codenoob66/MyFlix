89b0cd844a72c2b72c95e7f07ca772ada1577306
rafael vincent <d749107@gmail.com>
2025-10-02 01:58:55 +0000

    temporary

--- Client/src/pages/Homepage.jsx
+++ Client/src/pages/Homepage.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import VideoPlayer from "../components/VideoPlayer";
 import MovieCatalog from "../components/MovieCatalog";
-import { useEffect } from "react";
+import { useEffect, useRef} from "react";
 
 const Homepage = () => {
   const BASE_URL = "https://myflix-ff2q.onrender.com/"
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [movies, setMovies] = useState([])
+  const inputRef = useRef(null)
 
   const handlePlay = (movie) => {
     setSelectedMovie(movie);
+  };
+
+  const handleFocus = () => {
+    if(inputRef.current){
+      inputRef.current.focus()
+    }
   };
 
   async function getMovies() {
@@ -29,7 +36,7 @@ useEffect(() => {
     <div className="p-6 bg-black text-white min-h-screen">
     
 
-      <section className="mb-10">
+      <section className="mb-10" ref={inputRef}>
         <VideoPlayer src={selectedMovie?.src} />
       </section>
 
@@ -38,6 +45,7 @@ useEffect(() => {
           title="Available Movies today"
           movies={movies}
           onSelect={handlePlay} // Pass the function to update selectedMovie
+          onClick={handleFocus}
         />
       </section>
     </div>
