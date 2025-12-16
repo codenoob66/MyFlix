168062180ec481ab0947fb44db586bdbc213ab44
rafael vincent <d749107@gmail.com>
2025-12-16 12:51:57 +0800

    Update BASE_URL to use HTTPS for API

--- Client/src/pages/Homepage.jsx
+++ Client/src/pages/Homepage.jsx
@@ -4,7 +4,7 @@ import MovieCatalog from "../components/MovieCatalog";
 import { useEffect, useRef} from "react";
 
 const Homepage = () => {
-  const BASE_URL = "http://85.190.102.228:5000/"
+  const BASE_URL = "https://myflixapi.codesbyrafael.com"
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [movies, setMovies] = useState([])
   const targetRef = useRef(null)
